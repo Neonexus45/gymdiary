3568dc836f80c54c4778a38a4918fefd15a27db3
ethan <ethan.tomaso@gmail.com>
2022-03-25 02:34:08 +0100

    finishing the database system and the login/inscription system

--- app/src/main/java/com/example/gymdiary/Login.java
+++ app/src/main/java/com/example/gymdiary/Login.java
@@ -2,24 +2,64 @@ package com.example.gymdiary;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.button.MaterialButton;
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
+
+import javax.net.ssl.*;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
 
 public class Login extends AppCompatActivity {
 
 
-    public void toRegister(){
-        Intent intent = new Intent(this,Register.class);
-        startActivity(intent);
-    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //BYPASS POUR LA CLE SLL A RETIRER SI DEPLOYEMENT (ILLEGAL LOL)
+        try {
+            TrustManager[] victimizedManager = new TrustManager[]{
+
+                    new X509TrustManager() {
+
+                        public X509Certificate[] getAcceptedIssuers() {
+
+                            X509Certificate[] myTrustedAnchors = new X509Certificate[0];
+
+                            return myTrustedAnchors;
+                        }
+
+                        @Override
+                        public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                        }
+
+                        @Override
+                        public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                        }
+                    }
+            };
+
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, victimizedManager, new SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+            HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String s, SSLSession sslSession) {
+                    return true;
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         TextView username = (TextView) findViewById(R.id.username);
         TextView password = (TextView) findViewById(R.id.password);
@@ -27,16 +67,62 @@ public class Login extends AppCompatActivity {
         MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
         MaterialButton regbtn = (MaterialButton) findViewById(R.id.regbtn);
 
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+                //gestion de la connexion
+                if(!username.getText().toString().equals("") && !password.getText().toString().equals(""))
+
+                {
+                    Handler handler = new Handler(Looper.getMainLooper());
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            String[] field = new String[2];
+                            field[0] = "username";
+                            field[1] = "password";
+                            String[] data = new String[2];
+                            data[0] = username.getText().toString();
+                            data[1] = password.getText().toString();
+                            PutData putData = new PutData("https://192.168.1.31/gymdiary/login.php", "POST", field, data);
+                            if (putData.startPut()) {
+                                if (putData.onComplete()) {
+                                    String result = putData.getResult();
+                                    if(result.equals("Login Success")){
+                                        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                    else{
+                                        Toast.makeText(getApplicationContext(),result,Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            }
+                        }
+                    });
+
+                }
+
+                else{
+                    Toast.makeText(getApplicationContext(),"Veuillez remplir tout les champs",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+        });
+
+
+        //redirection pour inscription
         regbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
-                    toRegister();
-
-                } else {
+                Intent intent = new Intent(getApplicationContext(),Register.class);
+                startActivity(intent);
 
 
-                }
             }
         });
 
--- app/src/main/java/com/example/gymdiary/Register.java
+++ app/src/main/java/com/example/gymdiary/Register.java
@@ -1,15 +1,64 @@
 package com.example.gymdiary;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.button.MaterialButton;
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
+
+import javax.net.ssl.*;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
 
 public class Register extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+        //BYPASS POUR LA CLE SLL A RETIRER SI DEPLOYEMENT (ILLEGAL LOL)
+        try {
+            TrustManager[] victimizedManager = new TrustManager[]{
+
+                    new X509TrustManager() {
+
+                        public X509Certificate[] getAcceptedIssuers() {
+
+                            X509Certificate[] myTrustedAnchors = new X509Certificate[0];
+
+                            return myTrustedAnchors;
+                        }
+
+                        @Override
+                        public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                        }
+
+                        @Override
+                        public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                        }
+                    }
+            };
+
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, victimizedManager, new SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+            HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String s, SSLSession sslSession) {
+                    return true;
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
@@ -17,19 +66,63 @@ public class Register extends AppCompatActivity {
         TextView username = (TextView) findViewById(R.id.username);
         TextView password = (TextView) findViewById(R.id.password);
         TextView email = (TextView) findViewById(R.id.email);
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.progress);
 
-        MaterialButton regbtn = (MaterialButton) findViewById(R.id.regbtn);
+        MaterialButton regbtn = (MaterialButton) findViewById(R.id.regbtnrredirect);
 
-
+        //gestion inscription a l'application
         regbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(username.getText().toString().equals("") && password.getText().toString().equals("") && email.getText().toString().equals("")){
 
+
+
+                //gestion de la connexion
+                if(!username.getText().toString().equals("") && !password.getText().toString().equals("") && !email.getText().toString().equals(""))
+
+                {
+                    progressBar.setVisibility(View.VISIBLE);
+                    Handler handler = new Handler(Looper.getMainLooper());
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            String[] field = new String[3];
+                            field[0] = "username";
+                            field[1] = "password";
+                            field[2] = "email";
+                            String[] data = new String[3];
+                            data[0] = username.getText().toString();
+                            data[1] = password.getText().toString();
+                            data[2] = email.getText().toString();
+                            PutData putData = new PutData("https://192.168.1.31/gymdiary/signup.php", "POST", field, data);
+                            if (putData.startPut()) {
+                                if (putData.onComplete()) {
+                                    progressBar.setVisibility(View.GONE);
+                                    String result = putData.getResult();
+                                    if(result.equals("Sign Up Success")){
+                                        Intent intent = new Intent(getApplicationContext(),Login.class);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                    else{
+                                        Toast.makeText(getApplicationContext(),result,Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            }
+                        }
+                    });
 
                 }
 
+                else{
+                    Toast.makeText(getApplicationContext(),"Veuillez remplir tout les champs",Toast.LENGTH_SHORT).show();
+                }
+
             }
+
         });
+
+
+
     }
 }
